033812963dc51a0e0ae57e56e0dcfa8bffbf07e0
Alexis GRIMALDI <alexis.grimaldi@gmail.com>
2012-09-16 16:07:41 +0200

    Option for bin / essay

--- parse.py
+++ parse.py
@@ -25,8 +25,11 @@ def parseLine(line):
 def mean(values):
     return sum(values) / float(len(values))
 
+def space(line):
+	av = mean(line[9:])
+	return av
 
-def meanSession(line):
+def meanSession(line, nbin=60):
     '''Computes the mean of the bins of each trials for the mean_session
     '''
     # Recuperation des valeurs "bin"
@@ -34,6 +37,8 @@ def meanSession(line):
     # Initialize dont la longueur = nombres d'essais
     essays = {}
     means = []
+    #Donne le numero du sujet
+    rat = line[6]
     # Assigne un bin à l'essai correspondant (un essai = 60 bins)
     # dans un dictionnaire (structure de donnee 'cle : valeur')
     # A la fin,
@@ -43,31 +48,35 @@ def meanSession(line):
     #    3: ...
     # }
     for i, bin in enumerate(session):
-        e = i / 60
+        e = i / nbin
         if e in essays:
             essays[e].append(bin)
         else:
             essays[e] = [bin]
-    for i in range(60):
+    for i in range(nbin):
         means.append(mean([v[i] for v in essays.values()]))
-    return means
+    return [rat,means]
 
 
 def main():
 
     # Check the number of command line arguments
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 3:
 
         # Open the file in readonly mode
         # this returns a file object
         myfile = open(sys.argv[1], 'r')
-
+	
+	# Specify bin count / essay as a third argument
+        nbin = int(sys.argv[2])
+        
         # Iterate over each line of the given file object
         for line in myfile:
             columns = parseLine(line)
             if columns[0] != 'Project':
-                mean_session = meanSession(columns)
-                print mean_session
+                subject, mean_session = meanSession(columns, nbin)
+		spatial = space(columns)
+                print str(subject) + '\t' + str(spatial) + '\t' + '\t'.join(map(str, mean_session))  
 
     else:
         print "Nombre d'arguments insatisfaisant : ./parse.py <nom du fichier>"
