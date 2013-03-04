febb707fb7c1d147b63ab4d4bd8e5b590ca08a0d
auroremalet <aurore.malet@gmail.com>
2013-03-04 22:41:23 +0100

    reads the file per selected date in all data

--- spatial_learning_by_hole.R
+++ spatial_learning_by_hole.R
@@ -1,6 +1,7 @@
 options(width=230)
 library(reshape2)
 library(ggplot2)
+library(grid)
 library(plyr)
 
 std = function(x, s) {
@@ -14,18 +15,20 @@ m.mean = function(x, s) {
 }
 
 #a = read.table('./aftershiftA.txt', header=F, sep='\t')
-a = read.table('./aftershiftB.txt', header=F, sep='\t')
+a = read.table('./All_Data_B.txt', header=F, sep='\t')
 
-#whitelist = c('23/06/2010')
-holes = c(1, 2, 3, 4, 5)
+whitelist = c('21/06/2010', '23/06/2010')
+contexts = c('B')
+holes = c(1)
 ctrl = c(5, 9, 11, 8, 10, 12)
 exptl = c(1, 3, 7, 2, 4, 6)
 
 bin.names = c()
 for (i in 1:(ncol(a)-11)) { bin.names = c(bin.names, i) }
 colnames(a) = c('context', 'hole', 'env', 'user', 'prog', 'session', 'cage', 'pass', 'ratID', 'Date', 'hour', bin.names)
+a = a[a$context %in% contexts, ]
 a = a[a$hole %in% holes, ]
-#a = a[a$Date %in% whitelist, ]
+a = a[a$Date %in% whitelist, ]
 a$Date = as.Date(a$Date, format='%d/%m/%Y')
 a$group = 'ctrl'
 a[a$ratID %in% exptl, ]$group = 'exptl'
@@ -50,23 +53,32 @@ print(head(d))
 colnames(d) = c('hole', 'group', 'Date', 'mean', 'sem')
 
 
+d$Date = factor(d$Date)
 
 myplot = ggplot(data=d, aes(x=Date, y=mean, fill=factor(group, labels=c('Control', 'Shifted')))) +
-  geom_bar(color='black', position='dodge', stat='identity') +
+  geom_bar(position='dodge', stat='identity') +
+  geom_bar(color='black', position='dodge', stat='identity', show_guide=FALSE) +
   geom_errorbar(aes(ymin=mean-sem, ymax=mean+sem), width=0.25, position=position_dodge(width=0.9)) +
-  facet_wrap(~ hole) +
-  xlab('Hole') + ylab('Entry / second') +
+  #facet_grid(Date ~ hole) +
+  scale_x_discrete(labels=c('Before','After')) +
+  xlab('') + ylab('Mean Entry / second') +
   scale_fill_grey(start=0.9, end=0.3) +
   theme_bw() +
   theme(
     panel.grid.major.x = element_blank(),
     panel.grid.minor = element_blank(),
-    axis.title = element_text(size=20),
-    axis.title.x = element_text(vjust=-0.3),
+    panel.grid = element_blank(),
+    axis.title = element_text(size=24),
+    #axis.text.x = element_text(vjust=-0.4),
+    axis.ticks.margin = unit(5, 'mm'),
+    #axis.text.x = element_blank('x','y'),
     axis.title.y = element_text(vjust=0.2),
-    axis.text = element_text(size=18),
+    axis.text = element_text(size=20),
     legend.title = element_blank(),
-    legend.text = element_text(size=18)
+    legend.key = element_rect(colour = 'black'),
+    legend.text = element_text(size=20)
   )
 
 print(myplot)
+hole = holes[1]
+ggsave(paste('spatial_learning_BeforeAfter_B', hole, '.png', sep='_'), scale=2)
